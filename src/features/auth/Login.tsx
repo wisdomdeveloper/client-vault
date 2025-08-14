5bb7bf3cdb992e40822fce247dbe115afd7b0064
Wisdom Ekene <wisdom12eken@gmail.com>
2025-08-14 12:59:34 +0100

    The Switch

--- src/features/auth/Login.tsx
+++ src/features/auth/Login.tsx
@@ -1,6 +1,12 @@
 "use client";
 
+import { signIn } from "next-auth/react";
+
 const LogIn = () => {
+  const handleSignIn = async () => {
+    signIn("google", { callbackUrl: "/dashboard" });
+  };
+
   return (
     <div className="w-full h-screen overflow-y-hidden flex items-center">
       <div className="p-10 mx-auto max-w-[500px] border border-[var(--border-color)] rounded-[8px]">
@@ -14,7 +20,10 @@ const LogIn = () => {
         </div>
         <div className="mx-auto flex justify-center">
           <button></button>
-          <button className="border border-[var(--border-color)] px-8 py-3 text-[length:var(--body-s)] font-[500] hover:bg-white hover:text-black text-[var(--text-primary)] rounded-[4px] mt-8 transition cursor-pointer w-full">
+          <button
+            onClick={handleSignIn}
+            className="border border-[var(--border-color)] px-8 py-3 text-[length:var(--body-s)] font-[500] hover:bg-white hover:text-black text-[var(--text-primary)] rounded-[4px] mt-8 transition cursor-pointer w-full"
+          >
             Sign in
           </button>
         </div>
